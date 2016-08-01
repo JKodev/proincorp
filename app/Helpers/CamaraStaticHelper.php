824c17b3146ce7bdfa0deaeea8875c5355b7cbc1
Kodev <jonadotnet@gmail.com>
2016-08-01 00:45:41 -0500

    Added XML reader

--- app/Helpers/CamaraStaticHelper.php
+++ app/Helpers/CamaraStaticHelper.php
@@ -9,6 +9,7 @@ class CamaraStaticHelper
 	public static function getZones($camara_id)
 	{
 		$detector_config = DetectorConfig::where('detectorId', $camara_id)->first();
-		dd($detector_config->content);
+		$xml = new \SimpleXMLElement($detector_config->content);
+		dd($xml);
 	}
 }
