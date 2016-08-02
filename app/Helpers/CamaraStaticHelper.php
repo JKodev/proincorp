a7d4df5eb526144f5a5c3ced945fae9995492979
Kodev <jonadotnet@gmail.com>
2016-08-02 17:55:39 -0500

    Added Zones to Detector

--- app/Helpers/CamaraStaticHelper.php
+++ app/Helpers/CamaraStaticHelper.php
@@ -9,6 +9,8 @@ class CamaraStaticHelper
 	public static function getZones($camara_id)
 	{
 		$detector_config = DetectorConfig::where('detectorId', $camara_id)->first();
+		$xmls = XmlParser::extract($detector_config->content);
+		dd($xmls);
 		dd($detector_config->toArray());
 		$xml = new \SimpleXMLElement($detector_config->content);
 		$zones = $xml->TrafficData->TrafficData->Zones->Zone;
