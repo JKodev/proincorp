2d73a5a3281fec39f462d6e24f220ff72e1eac42
Kodev <jonadotnet@gmail.com>
2016-08-11 15:59:16 -0500

    Added seed of admin user to app.

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,6 +11,11 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
+    	DB::table('users')->insert([
+    		'name'  =>  'admin',
+		    'email' =>  'admin@proincorp.pe',
+		    'password'  =>  bcrypt('admin123')
+	    ]);
         // $this->call(UsersTableSeeder::class);
     }
 }
