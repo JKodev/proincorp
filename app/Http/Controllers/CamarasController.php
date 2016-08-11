9ea08b999bfed275c2d32f9c89107fc7972d3703
Kodev <jonadotnet@gmail.com>
2016-08-11 13:21:46 -0500

    Added colors to Camera Controller

--- app/Http/Controllers/CamarasController.php
+++ app/Http/Controllers/CamarasController.php
@@ -10,12 +10,14 @@ use App\Http\Requests;
 
 class CamarasController extends Controller
 {
+	private $colors = ["green-jungle", "blue-sharp", "red-thunderbird", "yellow-gold", "purple-seance", "blue-ebonyclay", "green-turquoise", "grey-salsa", "red-sunglo", "yellow-soft", "purple-medium"];
     public function index()
     {
     	$camaras = Camara::where('currentConfigurationIndex', -99)->get();
 
 	    return view('app.camaras.index', [
-	    	'camaras'   =>  $camaras
+	    	'camaras'   =>  $camaras,
+		    'colors'    =>  $this->colors
 	    ]);
     }
 
@@ -36,7 +38,8 @@ class CamarasController extends Controller
 
 	    return view('app.camaras.show', [
 		    'camara'    =>  $camara,
-		    'ubicacion' =>  $ubicacion
+		    'ubicacion' =>  $ubicacion,
+		    'colors'    =>  $this->colors
 	    ]);
     }
 }
