61feb80d8d9300bcb689f7b6f355ea91244c5ce1
Kodev <jonadotnet@gmail.com>
2016-08-15 19:32:13 -0500

    Testing: debugging query.

--- app/Http/Controllers/PorticoController.php
+++ app/Http/Controllers/PorticoController.php
@@ -257,6 +257,8 @@ class PorticoController extends Controller
 			'sentido'   =>  preg_replace('/(\d+)\_(\d+)/', " ", $lector->dsc_lector_movimiento)
 		);
 
+		dd($data['registers']);
+
 		Excel::create($data['title'], function(LaravelExcelWriter $excel) use ($data) {
 			$excel->sheet('Vehiculos Dia', function(LaravelExcelWorksheet $sheet) use ($data) {
 				$sheet->loadView('app.portico.report.excel.third', $data);
